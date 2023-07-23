8930472583a7e7543ab662b8fdaa68cf590617c5
Edson Michaque <edson@michaque.com>
2023-07-23 21:55:48 +0200

    Flatten the api structure

--- api/v1alpha1/connectord_grpc.pb.go
+++ api/v1alpha1/connectord_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.2.0
 // - protoc             (unknown)
-// source: api/connectord/v1alpha1/connectord.proto
+// source: api/v1alpha1/connectord.proto
 
 package connectordv1alpha1
 
@@ -207,5 +207,5 @@ var Connectord_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "api/connectord/v1alpha1/connectord.proto",
+	Metadata: "api/v1alpha1/connectord.proto",
 }
