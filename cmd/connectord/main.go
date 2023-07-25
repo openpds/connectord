f8a6cf12f97d399e755265c6bea8a026b45365c3
Edson Michaque <edson@michaque.com>
2023-07-25 16:19:52 +0200

    Add ConnectorWrapper type

--- cmd/connectord/main.go
+++ cmd/connectord/main.go
@@ -19,7 +19,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"time"
 
 	"github.com/openpds/connectord/config"
 	connector "github.com/openpds/connectord/connector"
@@ -27,175 +26,16 @@ import (
 )
 
 func main() {
-	reg := connector.NewRegistry(dummymz.New())
+	connectors := connector.NewRegistry(dummymz.New())
 
-	cfg, err := config.Init(reg)
+	cfg, err := config.Init(connectors)
 	if err != nil {
 		panic(err)
 	}
 
 	log.Printf("%q", cfg)
 
-	reg.Walk(func(c connector.Connector) {
+	connectors.Walk(func(c connector.Connector) {
 		fmt.Printf("ID: %s\nNAME: %s\nVERSION: %s\n", c.Manifest().ID, c.Manifest().Name, c.Manifest().Version)
 	})
-
-	ticker := time.NewTicker(10 * time.Second)
-
-	for {
-		select {
-		case t := <-ticker.C:
-			run(t)
-		}
-	}
 }
-
-func run(t time.Time) {
-	fmt.Println(t)
-}
-
-/*func doConfigure(ctx context.Context, connctx *connector.Context, conn connector.Connector) error {
-	if err := ctx.Err(); err != nil {
-		return err
-	}
-
-	ctx, cancel := context.WithTimeout(ctx, time.Second)
-	defer cancel()
-
-	errC := make(chan error)
-	defer close(errC)
-
-	go func() {
-		if err := conn.Configure(connctx); err != nil {
-			errC <- err
-		}
-	}()
-
-	select {
-	case <-ctx.Done():
-		return context.Canceled
-	case err := <-errC:
-		return err
-	}
-}
-
-func (Connectord) doCreateTransfer(ctx context.Context, c connector.Connector, input *connector.CreateTransferInput) (*connector.CreateTransferOutput, error) {
-	if err := ctx.Err(); err != nil {
-		return nil, err
-	}
-
-	errC := make(chan error)
-	defer close(errC)
-
-	respC := make(chan *connector.CreateTransferOutput)
-	defer close(respC)
-
-	go func() {
-		creator, ok := c.(connector.TransferCreator)
-		if !ok {
-			errC <- connector.ErrMotImplemented
-			return
-		}
-
-		resp, err := creator.CreateTransfer(ctx, input)
-		if err != nil {
-			errC <- err
-			return
-		}
-
-		respC <- resp
-	}()
-
-	select {
-	case <-ctx.Done():
-		return nil, context.Canceled
-	case err := <-errC:
-		return nil, err
-	case resp := <-respC:
-		return resp, nil
-	}
-}
-
-func (Connectord) doConfirmTransfer(ctx context.Context, c connector.Connector, input *connector.CreateTransferInput) (*connector.CreateTransferOutput, error) {
-	if err := ctx.Err(); err != nil {
-		return nil, err
-	}
-
-	errC := make(chan error)
-	defer close(errC)
-
-	respC := make(chan *connector.CreateTransferOutput)
-	defer close(respC)
-
-	go func() {
-		creator, ok := c.(connector.TransferCreator)
-		if !ok {
-			errC <- connector.ErrMotImplemented
-			return
-		}
-
-		resp, err := creator.CreateTransfer(ctx, input)
-		if err != nil {
-			errC <- err
-			return
-		}
-
-		respC <- resp
-	}()
-
-	select {
-	case <-ctx.Done():
-		return nil, context.Canceled
-	case err := <-errC:
-		return nil, err
-	case resp := <-respC:
-		return resp, nil
-	}
-}
-
-func (Connectord) doCancelTransfer(ctx context.Context, c connector.Connector, input *connector.CreateTransferInput, opts ...connector.Option) (*connector.CreateTransferOutput, error) {
-	if err := ctx.Err(); err != nil {
-		return nil, err
-	}
-
-	o := connector.Options{}
-
-	for _, opt := range opts {
-		opt(&o)
-	}
-
-	ctx, cancel := context.WithTimeout(ctx, o.Timeout)
-	defer cancel()
-
-	errC := make(chan error)
-	defer close(errC)
-
-	respC := make(chan *connector.CreateTransferOutput)
-	defer close(respC)
-
-	go func() {
-		creator, ok := c.(connector.TransferCreator)
-		if !ok {
-			errC <- connector.ErrMotImplemented
-			return
-		}
-
-		resp, err := creator.CreateTransfer(ctx, input)
-		if err != nil {
-			errC <- err
-			return
-		}
-
-		respC <- resp
-	}()
-
-	select {
-	case <-ctx.Done():
-		return nil, context.Canceled
-	case err := <-errC:
-		return nil, err
-	case resp := <-respC:
-		return resp, nil
-	}
-}
-*/
---
+++ cmd/connectord/start.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/openpds/connectord/connector"
+)
+
+func NewConnectorWrapper(reg *connector.Registry, name string) (*ConnectorWrapper, error) {
+	conn, err := reg.Find(name)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ConnectorWrapper{conn}, nil
+}
+
+type ConnectorWrapper struct {
+	conn connector.Connector
+}
+
+func (c ConnectorWrapper) Configure(ctx context.Context, cfg *connector.ConfigureOptions) error {
+	if err := ctx.Err(); err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second)
+	defer cancel()
+
+	errC := make(chan error)
+	defer close(errC)
+
+	go func() {
+
+		if err := c.conn.Configure(cfg); err != nil {
+			errC <- err
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+		return context.Canceled
+	case err := <-errC:
+		return err
+	}
+}
+
+func (c ConnectorWrapper) CreateTransfer(ctx context.Context, input *connector.CreateTransferInput) (*connector.CreateTransferOutput, error) {
+	if err := ctx.Err(); err != nil {
+		return nil, err
+	}
+
+	errC := make(chan error)
+	defer close(errC)
+
+	respC := make(chan *connector.CreateTransferOutput)
+	defer close(respC)
+
+	go func() {
+		creator, ok := c.conn.(connector.TransferCreator)
+		if !ok {
+			errC <- connector.ErrNotImplemented
+			return
+		}
+
+		resp, err := creator.CreateTransfer(ctx, input)
+		if err != nil {
+			errC <- err
+			return
+		}
+
+		respC <- resp
+	}()
+
+	select {
+	case <-ctx.Done():
+		return nil, context.Canceled
+	case err := <-errC:
+		return nil, err
+	case resp := <-respC:
+		return resp, nil
+	}
+}
+
+func (c ConnectorWrapper) ConfirmTransfer(ctx context.Context, input *connector.CreateTransferInput) (*connector.CreateTransferOutput, error) {
+	if err := ctx.Err(); err != nil {
+		return nil, err
+	}
+
+	errC := make(chan error)
+	defer close(errC)
+
+	respC := make(chan *connector.CreateTransferOutput)
+	defer close(respC)
+
+	go func() {
+		creator, ok := c.conn.(connector.TransferCreator)
+		if !ok {
+			errC <- connector.ErrNotImplemented
+			return
+		}
+
+		resp, err := creator.CreateTransfer(ctx, input)
+		if err != nil {
+			errC <- err
+			return
+		}
+
+		respC <- resp
+	}()
+
+	select {
+	case <-ctx.Done():
+		return nil, context.Canceled
+	case err := <-errC:
+		return nil, err
+	case resp := <-respC:
+		return resp, nil
+	}
+}
+
+func (c ConnectorWrapper) CancelTransfer(ctx context.Context, input *connector.CreateTransferInput, opts ...connector.Option) (*connector.CreateTransferOutput, error) {
+	if err := ctx.Err(); err != nil {
+		return nil, err
+	}
+
+	o := connector.Options{}
+
+	for _, opt := range opts {
+		opt(&o)
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, o.Timeout)
+	defer cancel()
+
+	errC := make(chan error)
+	defer close(errC)
+
+	respC := make(chan *connector.CreateTransferOutput)
+	defer close(respC)
+
+	go func() {
+		creator, ok := c.conn.(connector.TransferCreator)
+		if !ok {
+			errC <- connector.ErrNotImplemented
+			return
+		}
+
+		resp, err := creator.CreateTransfer(ctx, input)
+		if err != nil {
+			errC <- err
+			return
+		}
+
+		respC <- resp
+	}()
+
+	select {
+	case <-ctx.Done():
+		return nil, context.Canceled
+	case err := <-errC:
+		return nil, err
+	case resp := <-respC:
+		return resp, nil
+	}
+}
